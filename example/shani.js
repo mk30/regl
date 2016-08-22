2f6d7f1b805034f60fc0f81a6ce22ff9a29b0ef1
Marina Kukso <marina.kukso@gmail.com>
2016-08-22 11:53:43 -0700

    cyl.html + shani.js working with cyl and icecream modules. shani.js needs more demos and a way to step through demos.

--- example/shani.js
+++ example/shani.js
@@ -1,20 +1,18 @@
-document.body.onload = addElement;
-
-var frames = [
-  "http://kitties.neocities.org/4x4.html",
-  "http://kitties.neocities.org/icecream.html"
-]
-
-function addElement () { 
-  var newDiv = document.createElement("div"); 
-  var iframe = document.createElement("iframe"); 
-  var i = 0
-  setInterval(function(){
-    var frame = frames[i++%frames.length]
-    iframe.src = frame.toString()  
-  }, 2000)
-  iframe.style=
-    "border: 0; position:absolute; top:0; left:0; right:0; bottom:0; width:100%; height:100%"
-  newDiv.appendChild(iframe); 
-  document.body.appendChild(newDiv); 
+var regl = require('../regl')()
+var demos = {
+  cyl: require('./shanicyl.js')(regl),
+  icecream: require('./shaniicecream.js')(regl)
 }
+const camera = require('./util/camera')(regl, {
+  center: [0, 2.5, 0]
+})
+
+var active = 'cyl'
+regl.frame(() => {
+  regl.clear({
+    color: [0, 0, 0, 1]
+  })
+  camera(() => {
+    demos[active]()
+  })
+})
