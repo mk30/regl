42fb5451d57172652c6307d0bb7ebae6d85ae497
Marina Kukso <marina.kukso@gmail.com>
2016-08-22 20:53:29 -0700

    added 4x4 to this branch; added 4x4 to shani file

--- example/shani.js
+++ example/shani.js
@@ -1,13 +1,14 @@
 var regl = require('../regl')()
 var demos = {
   cyl: require('./shanicyl.js')(regl),
-  icecream: require('./shaniicecream.js')(regl)
+  icecream: require('./shaniicecream.js')(regl),
+  fourxfour: require('./shani4x4.js')(regl)
 }
 const camera = require('./util/camera')(regl, {
   center: [0, 2.5, 0]
 })
 
-var active = 'cyl'
+var active = 'fourxfour'
 regl.frame(() => {
   regl.clear({
     color: [0, 0, 0, 1]
