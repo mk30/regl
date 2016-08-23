ad0388026fbb461d6e22d708d22234de4374f913
Marina Kukso <marina.kukso@gmail.com>
2016-08-23 09:24:19 -0700

    setinterval working

--- example/shani.js
+++ example/shani.js
@@ -1,19 +1,25 @@
 var regl = require('../regl')()
-var demos = {
-  cyl: require('./shanicyl.js')(regl),
-  icecream: require('./shaniicecream.js')(regl),
-  fourxfour: require('./shani4x4.js')(regl)
-}
+var demos = [ 
+  require('./shanicyl.js')(regl),
+  require('./shaniicecream.js')(regl),
+  require('./shani4x4.js')(regl)
+]
 const camera = require('./util/camera')(regl, {
   center: [0, 2.5, 0]
 })
 
-var active = 'fourxfour'
+var index = 0
+
+var interval = setInterval(function() {
+  index++
+}, 3000)
+
+//var active = 'fourxfour'
 regl.frame(() => {
   regl.clear({
     color: [0, 0, 0, 1]
   })
   camera(() => {
-    demos[active]()
+    demos[index%demos.length]() 
   })
 })
