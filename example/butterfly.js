bcbd55ff89cac2216463e19f065839c36357d7f7
Marina Kukso <marina.kukso@gmail.com>
2016-08-25 12:55:19 -0700

    butterfly.js demo working.

--- example/butterfly.js
+++ example/butterfly.js
@@ -2,11 +2,12 @@ const regl = require('../regl')()
 const mat4 = require('gl-mat4')
 var rmat = []
 
-const cyl = require('./implicitcyl.js')
+const cyl = require('./butterflycyl.js')
+const cream = require('./butterflycream.js')
 const normals = require('angle-normals')
 
 const camera = require('./util/camera')(regl, {
-  center: [0, 2.5, 0]
+  center: [0.0, 2.5, 0.0]
 })
 
 const drawcyl = regl({
@@ -35,7 +36,7 @@ const drawcyl = regl({
       vnormal = normal;
       gl_Position = projection * view * model * vec4(warp(position), 1.0);
       gl_PointSize =
-      (64.0*(1.0+sin(t*20.0+length(position))))/gl_Position.w;
+      (20.0*(5.0+sin(t*20.0+length(position))))/gl_Position.w;
     }`,
   attributes: {
     position: cyl.positions,
@@ -47,8 +48,55 @@ const drawcyl = regl({
          return context.tick/1000
        },
     model: function(context, props){
-      var theta = context.tick/60
-      return mat4.rotateZ(rmat, mat4.identity(rmat), theta)
+      var theta = -context.tick/60
+      //return mat4.rotateY(rmat, mat4.identity(rmat), theta)
+      return mat4.scale(rmat, mat4.identity(rmat),
+      [Math.sin(10.0*context.time), 0.0, 2.7])
+    }
+    
+  },
+  primitive: "points"
+})
+const drawcream = regl({
+  frag: `
+    precision mediump float;
+    varying vec3 vnormal;
+    vec3 hsl2rgb(vec3 hsl) {
+      vec3 rgb = clamp( abs(mod(hsl.x*5.0+vec3(0.0,4.0,2.0),6.0)-3.0)-1.0, 0.0, 1.0 );
+      return hsl.z - hsl.y * (rgb-0.5)*(3.0-abs(2.0*hsl.y-1.0));
+    }
+    void main () {
+      gl_FragColor = vec4(hsl2rgb(abs(vnormal)), 1.0);
+    }`,
+  vert: `
+    precision mediump float;
+    uniform mat4 model, projection, view;
+    attribute vec3 position, normal;
+    varying vec3 vnormal;
+    uniform float t;
+    vec3 warp (vec3 p){
+      float r = length(p.zx*sin(t*p.yz));
+      float theta = atan(p.z, p.x);
+      return vec3 (r*cos(theta), p.y, r*sin(theta));
+    }
+    void main () {
+      vnormal = normal;
+      gl_Position = projection * view * model * vec4(warp(position), 1.0);
+      gl_PointSize =
+      (20.0*(5.0+sin(t*20.0+length(position))))/gl_Position.w;
+    }`,
+  attributes: {
+    position: cream.positions,
+    normal: normals(cream.cells, cream.positions)
+  },
+  elements: cream.cells,
+  uniforms: {
+    t: function(context, props){
+         return context.tick/1000
+       },
+    model: function(context, props){
+      var theta = context.tick/100
+      return mat4.rotateY(rmat, mat4.identity(rmat), theta)
     }
     
   },
