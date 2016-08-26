7aaab9ec8c658d370a5197567f59e8a669528d73
Marina Kukso <marina.kukso@gmail.com>
2016-08-26 09:12:33 -0700

    unholy rolling

--- example/skellypuff.js
+++ example/skellypuff.js
@@ -2,15 +2,14 @@ const regl = require('../regl')()
 const mat4 = require('gl-mat4')
 var rmat = []
 
-//const bunny = require('bunny')
-const bunny = require('./skelly.json')
+const skelly = require('./skelly.json')
 const normals = require('angle-normals')
 
 const camera = require('./util/camera')(regl, {
   center: [0, 2.5, 0]
 })
 
-const drawBunny = regl({
+const drawskelly = regl({
   frag: `
     precision mediump float;
     varying vec3 vnormal;
@@ -31,7 +30,7 @@ const drawBunny = regl({
       float r = length(p.zx);
       float theta = atan(p.z, p.x);
       return vec3 (r*cos(theta), p.y, r*sin(theta)) +
-      vnormal*(1.0+cos(40.0*t+p.y));
+      vnormal+(1.0+cos(40.0*t+p.z));
     }
     void main () {
       vnormal = normal;
@@ -41,10 +40,10 @@ const drawBunny = regl({
       (64.0*(1.0+sin(t*20.0+length(position))))/gl_Position.w;
     }`,
   attributes: {
-    position: bunny.positions,
-    normal: normals(bunny.cells, bunny.positions)
+    position: skelly.positions,
+    normal: normals(skelly.cells, skelly.positions)
   },
-  elements: bunny.cells,
+  elements: skelly.cells,
   uniforms: {
     t: function(context, props){
          return context.tick/1000
@@ -64,6 +63,6 @@ regl.frame(() => {
     color: [0, 0, 0, 1]
   })
   camera(() => {
-    drawBunny()
+    drawskelly()
   })
 })
